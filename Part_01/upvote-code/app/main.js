d0f051429d8b6bb1b187e8ddc8bf284d89cada9f
Ezio <assassinste@gmail.com>
2019-03-28 07:30:22 +0800

    Display an ordered list of submissions (based on `id`)

--- Part_01/upvote-code/app/main.js
+++ Part_01/upvote-code/app/main.js
@@ -1,6 +1,21 @@
-new Vue({
+new Vue ({
   el: '#app',
   data: {
     submissions: Seed.submissions, // Seed => seed.js
+  },
+  computed: {
+    // Q: Why here (coding this 'computed property' part)?
+    // A: For any complex logic, you should do it in here (rather than template).
+    sortedSubmissions () {
+      /*
+        Apparently, the `submissions` here is one coming from `data` part.
+        So this one here will always depend on it (& update when `xx in data` changes).
+
+        For me, this part is kinda like << Vue_inst.data.submissions >> (unverified).
+      */
+      return this.submissions.sort ((former, latter) => {
+        return latter.votes - former.votes;
+      });
+    },
   },
 });
