2c8d117e7341b0357292f01dc4acba3d51eea111
Ezio <assassinste@gmail.com>
2019-03-28 15:56:47 +0800

    Moving a big chunk of HTML into a (not yet) component

--- Part_01/upvote-code/app/main.js
+++ Part_01/upvote-code/app/main.js
@@ -1,3 +1,40 @@
+const submissionComponent = {
+  // Some rules for this `template` attr
+  // [1] It must be enclosed within A SINGLE root element (the 'div' here)
+  // [2]
+  template: `
+  <div style="display: flex; width: 100%"
+    <figure class="media-left">
+      <img class="image is-64x64" v-bind:src="single_sub.submissionImage">
+    </figure>
+    <div class="media-content">
+      <div class="content">
+        <p>
+          <strong>
+              <a v-bind:href="single_sub.url" class="has-text-info">
+                  {{ single_sub.title }}
+              </a>
+              <span class="tag is-small">#{{ single_sub.id }}</span>
+          </strong>
+          <br/>
+              {{ single_sub.description }}
+          <br/>
+          <small class="is-size-7">
+              Submitted by:
+              <img class="image is-24x24" v-bind:src="single_sub.avatar" alt="This is Daniel">
+          </small>
+        </p>
+      </div>
+    </div>
+    <div class="media-right">
+      <span class="icon is-small" v-on:click="upvote(single_sub.id)">
+        <i class="fas fa-thumbs-up"></i>&nbsp;
+        <strong class="has-text-info">{{ single_sub.votes }}</strong>
+      </span>
+    </div>
+  <div>`,
+};
+
 new Vue ({
   el: '#app',
   data: {
